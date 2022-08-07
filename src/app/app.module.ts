9b2885a555a6015cb4fbe821a19cdb181a7fc842
racalixta <ra.calixta@gmail.com>
2022-08-06 22:37:28 -0300

    pasta docs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CardComponent } from './components/card/card.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SkillsComponent,
+    ContactComponent,
+    ProjectsComponent,
+    HeaderComponent,
+    FooterComponent,
+    CardComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
